fd28b4bbcd725e33fe735cb7079b57551d27a47c
YoungMin KIM <47775179+winston1214@users.noreply.github.com>
2024-01-23 14:52:34 +0900

    Update run.sh

--- DiffuseIT-M/run.sh
+++ DiffuseIT-M/run.sh
@@ -3,4 +3,4 @@ echo $CUDA_VISIBLE_DEVICES
 echo $SLURM_NODELIST
 echo $SLURM_NODEID
 ml purge 
-python main.py -i "/scratch/winston1214/talmo/train_img/5750_A2LEBJJDE000589_1604808871900_2_TH.jpg"  --output_path "/home/winston1214/" -tg "/scratch/winston1214/talmo/train_img/0617_A2LEBJJDE000815_1603859729654_3_TH.jpg" --diff_iter 100 --timestep_respacing 200 --skip_timesteps 80 --model_output_size 512 --init_mask /scratch/winston1214/talmo/reverse_ensemble_train/5750_A2LEBJJDE000589_1604808871900_2_TH.jpg --use_range_restart True --use_colormatch True --use_noise_aug_all True --iterations_num 1 --output_file Sebum_DIFFIT_M.png
+python main.py -i $source  --output_path $output_path -tg $target --diff_iter 100 --timestep_respacing 200 --skip_timesteps 80 --model_output_size 256 --init_mask $mask --use_range_restart True --use_colormatch True --use_noise_aug_all True --iterations_num 1 --output_file $output_file_name
